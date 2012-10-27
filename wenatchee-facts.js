f29d0b3c9f3b3c1c40415cd7cb5171a39c1f9247
Duffy Tilleman <duffy.tilleman@gmail.com>
2012-10-27 00:10:48 -0700

    fixed random_fact to never return same fact as current fact

--- wenatchee-facts.js
+++ wenatchee-facts.js
@@ -13,34 +13,22 @@ Meteor.methods({
 });
 
 if (Meteor.isClient) {
-  Template.hello.greeting = function () {
-    return "Welcome to wenatchee-facts.";
-  };
-
-  Template.hello.events({
-    'click input' : function () {
-      // template data, if any, is available in 'this'
-      if (typeof console !== 'undefined')
-        console.log("You pressed the button");
-    }
-  });
 
   Template.fact.random_fact = function() {
       var facts = Facts.find({'_id': {'$ne': Session.get('previous_fact')}}, reactive=false).fetch();
       var random = Math.floor(Math.random() * facts.length);
       var fact = facts[random];
-      var unused = Session.get('fact_count');
       //var fact = Facts.findOne({}, skip=random);
       if (fact) {
-          //Session.set('previous_fact', fact._id);
+          Session.set('current_fact', fact._id);
           return fact.text;
       }
   };
 
   Template.fact.events({
       'click #new': function() {
-          Session.set('fact_count', Session.get('fact_count') + 1);
-      }
+          Session.set('previous_fact', Session.get('current_fact'));
+      },
   });
 
   Meteor.startup(function() {
